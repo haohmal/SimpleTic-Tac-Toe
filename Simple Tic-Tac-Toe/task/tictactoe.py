8683a62e571d259eeb890e89393a04ddbf06a6db
Andreas Eckerle <a.eckerle@iroka.de>
2021-08-31 21:18:13 +0200

    stage 2: generate field from input string

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,3 +1,6 @@
-print("X O X")
-print("X X O")
-print("O X O")
+in_string = input()
+
+print("-" * 8)
+for i in range(3):
+    print("| {0} {1} {2} |".format(in_string[i * 3], in_string[i * 3 + 1], in_string[i * 3 + 2]))
+print("-" * 8)
